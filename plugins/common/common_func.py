b46cf6783eb96f872200a107dd72fe90184701e7
footix01 <footix01@naver.com>
2024-09-14 20:32:31 +0900

    op_kwargs 테스트용 dag 업로드

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -2,7 +2,13 @@ def get_stfp():
     print("stfp 작업 시작")
 
 
-def regist(name, sex, *args):
+def regist(name, sex, *args, **kwargs):
     print(f'이름: {name}')
     print(f'성별: {sex}')
     print(f'기타: {args}')
+    email = kwargs['email'] or None
+    phone = kwargs['phone'] or None
+    if email:
+        print(email)
+    if phone:
+        print(phone)
