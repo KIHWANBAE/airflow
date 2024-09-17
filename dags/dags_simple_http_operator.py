447fc5c3ec87bdd3abc619c13e8198a2e861dcd2
footix01 <footix01@naver.com>
2024-09-17 09:57:16 +0900

    api 연습 업로드

---
+++ dags/dags_simple_http_operator.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.decorators import task
+import pendulum
+
+with DAG(
+    dag_id="dags_simple_http_operator",
+    schedule=None,
+    start_date=pendulum.datetime(2024, 9, 17, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+
+    '''서울시 공공데이터 부동산 정보'''
+    tb_real_estate_info = SimpleHttpOperator(
+        task_id = 'tb_real_estate_info',
+        http_conn_id = 'openapi.seoul.go.kr',
+        endpoint = '{{var.value.apikey_openapi_seoul}}/json/tbLnOpendataRtmsV/1/10/',
+        method = 'GET',
+        headers = {'Content-Type' : 'application/json',
+                   'charset' : 'utf-8',
+                   'Accept' : '*/*'}
+    )
+
+    @task(task_id = 'python_2') 
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        result = ti.xcom_pull(task_ids='tb_real_estate_info')
+        import json
+        from pprint import pprint
+
+        pprint(json.loads(result))
+
+    tb_real_estate_info >> python_2()
+
