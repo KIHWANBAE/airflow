47476010f7e286e2e577e83bb98571277d5d0391
footix01 <footix01@naver.com>
2024-09-20 10:36:09 +0900

    postgres 실습

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -10,27 +10,30 @@ with DAG(
 ) as dag:
     
 
-    def insrt_postgres(ip, port, dbname, user, passwd, **kwargs):
+    def insrt_postgres(postgres_conn_id, **kwargs):
         import psycopg2
         from contextlib import closing
+        from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-        with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
+        postgres_hook = PostgresHook(postgres_conn_id)
+
+        with closing(postgres_hook.get_conn()) as conn:
             with closing(conn.cursor()) as cursor:
                 dag_id=kwargs.get('ti').dag_id
                 task_id=kwargs.get('ti').task_id
                 run_id=kwargs.get('ti').run_id
-                msg = 'insert 수행'
+                msg = 'hook insert 수행'
                 sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
                 cursor.execute(sql,(dag_id,task_id,run_id,msg))
                 conn.commit()
 
-    insrt_postgres = PythonOperator(
-        task_id='insrt_postgres',
+    insrt_postgres_with_hook = PythonOperator(
+        task_id='insrt_postgres_with_hook',
         python_callable=insrt_postgres,
-        op_args=['172.28.0.3', '5432', 'kihwanb', 'kihwanb', 'kihwanb']
+        op_args={'postgres_conn_id':'conn-db-postgres-custom'}
     )
 
-    insrt_postgres
+    insrt_postgres_with_hook
 
                      
 
