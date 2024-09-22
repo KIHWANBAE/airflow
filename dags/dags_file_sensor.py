9b9b7a064298e9fc011692fd9e8bc890bbaff485
footix01 <footix01@naver.com>
2024-09-22 10:43:02 +0900

    sensor 실습

---
+++ dags/dags_file_sensor.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.sensors.filesystem import FileSensor
+import pendulum
+
+with DAG(
+    dag_id = 'dags_file_sensor',
+    start_date=pendulum.datetime(2024,9,22, tz='Asia/Seoul'),
+    schedule='0 7 * * *',
+    catchup=False
+) as dag:
+    tbRealEstateStatusNew_sensor = FileSensor(
+        task_id = 'tbRealEstateStatusNew_sensor',
+        fs_conn_id= 'conn_file_opt_airflow_files',
+        filepath= 'tbRealEstateStatusNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tbRealEstateStatusNew.csv'
+        recursive=False,
+        poke_interval=60,
+        timeout=60*60*24,
+        mode='reschedule'
+    )
+
+
